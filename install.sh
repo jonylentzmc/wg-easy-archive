9df21261b0a4a9aae244737156bce65d93368177
jonylentzmc <76884929+jonylentzmc@users.noreply.github.com>
2024-11-17 01:33:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # download and install Node.js (you may need to restart the terminal)
-nvm install 20
+nvm install 18
 # verifies the right Node.js version is in the environment
 node -v # should print `v18.20.5`
 # verifies the right npm version is in the environment
